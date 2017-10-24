ad8e75e60666b4261eb3619a1f19474a6407f27a
Carlos Peña <carlos@passaromx.com>
2017-10-23 23:04:37 -0500

    Implemented async/awai

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,44 @@
 var User = require('../models/user');
 
 module.exports = {
-	index: (req, res, next) => {
-		User.find({})
-			.then(users => {
-				res.status(200).json(users);
-			})
-			.catch(err => {
-				next(err)
-			});
+	// index: (req, res, next) => {
+	// 	User.find({})
+	// 		.then(users => {
+	// 			res.status(200).json(users);
+	// 		})
+	// 		.catch(err => {
+	// 			next(err)
+	// 		});
+	// },
+
+	index: async (req, res, next) => {
+		try {
+			const users = await User.find({});
+			res.status(200).json(users);
+		} catch(err) {
+			next(err);
+		}
+		
 	},
 
-	newUser: (req, res, next) => {
-		const newUser = new User(req.body);
-		newUser.save()
-			.then(user => {
-				res.status(201).json(user);
-			})
-			.catch(err => {
-				next(err)
-			});
+		// newUser: (req, res, next) => {
+	// 	const newUser = new User(req.body);
+	// 	newUser.save()
+	// 		.then(user => {
+	// 			res.status(201).json(user);
+	// 		})
+	// 		.catch(err => {
+	// 			next(err)
+	// 		});
+	// }
+
+	newUser: async (req, res, next) => {
+		try {
+			const newUser = new User(req.body);
+			const user = await newUser.save();
+			res.status(201).json(user);
+		} catch(err) {
+			next(err);
+		}
 	}
 };
